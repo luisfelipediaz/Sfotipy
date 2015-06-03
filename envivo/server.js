b781f6f2b112adf580ab0d8c12b8427795fed42c
luisfelipediaz <luis.felipe.diaz.v@gmail.com>
2015-06-03 07:25:38 -0500

    Se suben cambios en el puerto

--- envivo/server.js
+++ envivo/server.js
@@ -2,7 +2,7 @@ var http = require("http"),
 	st   = require("node-static"),
 	opts = { cache: false },
 	file = new st.Server('./', opts),
-	port = process.env.PORT || 8080;
+	port = process.env.PORT || 8089;
 //4347-15-0002006722
 http.createServer(function(req, res){
 	file.serve(req, res);
